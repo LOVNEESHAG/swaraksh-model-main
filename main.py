594af838d95bc931e51f80ff7bbb2ad06aea7134
LOVNEESHAG <lovneeshagrawal.cse28@jecrc.ac.in>
2026-09-12 02:35:31 +0530

    Deplyment

--- main.py
+++ main.py
@@ -25,8 +25,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:5173",
+        "https://swaraksh-gamma.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
